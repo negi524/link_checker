02b297cda7d5718806aff1d45350ed1b9c3f9f47
negi524 <egtnrumpbiizarre.info@gmail.com>
2020-09-15 10:24:23 +0900

    create script files

---
+++ crawl_with_requests.py
@@ -0,0 +1,10 @@
+
+def main():
+    """main関数
+    selemiumではなくrequestsモジュールを利用してスクレイピングを行う
+    """
+    print("hello, world")
+
+
+if __name__ == '__main__':
+    main()
---
+++ crawl_with_selemium.py
@@ -0,0 +1,9 @@
+
+def main():
+    """main関数
+    requestsではなくselemiumモジュールを利用してスクレイピングを行う
+    """
+    print("comming soon...")
+
+if __name__ == '__main__':
+    main()
